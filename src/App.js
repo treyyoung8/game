868b18b12e0f4e38169f2717c8dba0edcb6bf9fb
= <treyyoung8@gmail.com>
2018-10-12 10:24:52 -0600

    merge

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-=======
-import Cups from './components/Cups'; 
-import Header from './components/Header'; 
->>>>>>> 4264f0f247f94dec3f2ba628a64f62e243516f61
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-
-=======
-      <Header />
-      <Cups />
->>>>>>> 4264f0f247f94dec3f2ba628a64f62e243516f61
       </div>
     );
   }
